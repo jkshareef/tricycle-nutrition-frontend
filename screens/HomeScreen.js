b9c4aead9b286edde2347007781ceb73c15fab22
jkshareef <jamalkshareef@gmail.com>
2019-07-26 15:30:28 -0700

    added initial code for filtered search text field autocomplete

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,8 +1,9 @@
 import React, {Component} from 'react';
-import {View, Text, StyleSheet, Button, AsyncStorage, ART, TouchableHighlight} from 'react-native'
+import {View, Text, StyleSheet, Button, AsyncStorage, ART, TouchableHighlight, ListView} from 'react-native'
 import { createStackNavigator, createAppContainer } from 'react-navigation';
 import { TextInput } from 'react-native-gesture-handler';
 import { List, Checkbox } from 'react-native-paper';
+import RecentMeal from '../components/RecentMeal'
 
 
 export default class HomeScreen extends Component {
@@ -14,6 +15,8 @@ export default class HomeScreen extends Component {
       foodNames: null,
       foodItems: null,
       expanded: false
+      
+    
     }
     this.getRecent = this.getRecent.bind(this)
   }
@@ -27,6 +30,11 @@ export default class HomeScreen extends Component {
   }
 
   async componentDidMount() {
+    this.getRecent()
+    this.getFoodNames()
+    }
+
+  async getRecent() {
     const token = await this.getToken()
     const config = {
         headers: {
@@ -43,27 +51,22 @@ export default class HomeScreen extends Component {
       })
       .catch(error => console.log('Error: ', error))
     }
-
-    async getRecent() {
-      const token = await this.getToken()
-      const config = {
-          headers: {
-            'Content-Type' : 'application/json',
-            'Authorization' : 'Bearer ' + token
-          }
-        }
-        fetch("http://localhost:3000/api/v1/food/recent", config)
-        .then(resp => resp.json())
-        .then(json => {
-          this.setState({
-            compoundData: json
-          })
-        })
-        .catch(error => console.log('Error: ', error))
-      }
-    
+  
   
 
+  getFoodNames = () => {
+    
+    const config = {
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    }
+    fetch("http://localhost:3000/foodnames", config)
+    .then(resp => resp.json())
+    .then(json => {
+      return json
+    })
+  }
   
 
   getToken = async () => {
@@ -119,57 +122,92 @@ export default class HomeScreen extends Component {
     }
     fetch(`http://localhost:3000/api/v1/add/${query}`, config)
     .then(resp=>resp.json())
+    .then(this.setState({
+      query: '',
+    }))
+    .then(this.getRecent())
     .catch(error => console.log("Error: ", error))
   }
 
-  recentMeal = () => {
-        result = []
-        // console.log(Object.keys(this.state.compoundData).length)
-          for (let i = 0; i < Object.keys(this.state.compoundData[data]).length; i++) {
-            result.push({
-              name: this.state.compoundData[Object.keys(this.state.compoundData)[i]].name, 
-              amount: this.state.compoundData[Object.keys(this.state.compoundData)[i]].amount,
-              rdv: this.state.compoundData[Object.keys(this.state.compoundData)[i]].rdv,
-              description: this.state.compoundData[Object.keys(this.state.compoundData)[i]].description
-            })
-            }
+  // recentMeal = () => {
+  //       result = []
+  //       // console.log(Object.keys(this.state.compoundData).length)
+  //         for (let i = 0; i < Object.keys(this.state.compoundData[data]).length; i++) {
+  //           result.push({
+  //             name: this.state.compoundData[Object.keys(this.state.compoundData)[i]].name, 
+  //             amount: this.state.compoundData[Object.keys(this.state.compoundData)[i]].amount,
+  //             rdv: this.state.compoundData[Object.keys(this.state.compoundData)[i]].rdv,
+  //             description: this.state.compoundData[Object.keys(this.state.compoundData)[i]].description
+  //           })
+  //           }
            
        
-       }
+  //      }
 
-       mealNames = () => {
-        for (let food in this.state.compoundData.data) {
-          // console.log(typeof this.state.compoundData.data[food])
-          // console.log(this.state.compoundData.data[Object.keys.this.state.compoundData.data.indexOf(food)])
-          return(
-          <List.Accordion
-          title={food}
-          style={{width: 400, alignItems: "center"}}
-          left={props => <List.Icon {...props} icon="restaurant" />}
-          expanded={this.state.expanded}
-          onPress={this.handlePress}
-        >
-          {this.mealCompounds(this.state.compoundData.data[food])}
-        </List.Accordion>
-          )}
+  mealNames = () => {
+        
+    for (let food in this.state.compoundData.data) {
+      // console.log(typeof this.state.compoundData.data[food])
+      // console.log(this.state.compoundData.data[Object.keys.this.state.compoundData.data.indexOf(food)])
+      return(
+      <List.Accordion
+        title={food}
+        style={{width: 400, alignItems: "center"}}
+        left={props => <List.Icon {...props} icon="restaurant" />}
+        expanded={this.state.expanded}
+        onPress={this.handlePress}
+      >
+        {this.mealCompounds(this.state.compoundData.data[food])}
+    </List.Accordion>
+      )}
+    
+  }
 
-        }
+  mealCompounds = (food) => {
+      
+      return food.map((compound, index) => {
+         return (
+        <List.Item
+          key= {index} 
+          title={compound.name}
+          description={`${compound.amount}/${compound.rdv} \n${compound.description}`}
+          // left={props => <List.Icon {...props} icon="arrow_right" />}
+      />    
+        )
+      }
+      )
+    }
+       
+    
+    renderFoodNames = (food) => {
+      return (
+        <View>
+          <Text>{food.name}</Text>
+        </View>
+      )
+    }
 
-        mealCompounds = (food) => {
-          
-            return food.map((compound) => {
-               return (
-              <List.Item 
-              title={compound.name}
-              description={`${compound.amount}/${compound.rdv} \n${compound.description}`}
-              // left={props => <List.Icon {...props} icon="arrow_right" />}
-            />    
-              )
-            }
-            )
-          }
+    searchedFoodNames = (text) => {
+      this.setState({
+        query: text
+      })
+      
+      let foodNames = this.getFoodNames().food_item
+      let names = foodNames.filter((foodName) => {
+        return foodName.name.toLowerCase().includes?(text)
+      })
+      this.setState({
+        filteredNames: names
+      })
+    }
+       
 
     render() {
+
+
+      
+      
+
         const styles = StyleSheet.create({
             container: {
               flex: 1,
@@ -203,33 +241,14 @@ export default class HomeScreen extends Component {
           }
           });
 
+        
+        let ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
           
-            
-              
-         
-       
-
-          // const {
-          //   Surface,
-          //   Group,
-          //   Shape,
-          // } = ART;
-          
-          // const ReactNativeART = 
-          //     <View>
-          //       <Surface width={500} height={500}>
-          //         <Group x={100} y={0}>
-          //           <Shape
-          //             d="M10 80 C 40 10, 65 10, 95 80 S 150 150, 180 80"
-          //             stroke="#000"
-          //             strokeWidth={1}/>
-          //         </Group>
-          //       </Surface>
-          //     </View>
+    
 
         return(
             <View style={styles.container}>
-              {/* {ReactNativeART} */}
+             
 
               <List.Section>
               <List.Accordion
@@ -239,45 +258,42 @@ export default class HomeScreen extends Component {
                   expanded={this.state.expanded}
                   onPress={this.handlePress}
                 >
-                  {this.state.compoundData === null?null:this.mealNames()}
+                  
+                  {this.state.compoundData? <RecentMeal meals={this.state.compoundData}/> : null}
+                  
                 </List.Accordion>
     
               </List.Section>
-
-              {/* <Text style={{fontSize:20}}>Most Recent Food</Text>
-              <Text>{this.state.compoundName === null?null:this.state.compoundName}</Text> */}
+              <View>
+                <TextInput style={styles.textField}
+                placeholder="Add Meal"
+                value={this.state.query}
+                onChangeText={(text) => this.setState({
+                  query: text})}
+                />
+                <ListView
+                      dataSource={ds.cloneWithRows(this.state.filteredNames)}
+                renderRow={this.renderFoodNames} />
+              </View>
               
-              <TextInput style={styles.textField}
-              placeholder="Add Meal"
-              value={this.state.query}
-              onChangeText={(text) => this.setState({query: text})}
-              />
               <TouchableHighlight onPress={this.onAddMeal} underlayColor="white">
                 <View style={styles.button}>
                   <Text style={styles.buttonText}>Add Meal</Text>
                 </View>
               </TouchableHighlight>
-              {/* <Button
-              style={styles.button}
-              title="Add Meal"
-              onPress={this.onAddMeal}/> */}
+          
               <TouchableHighlight onPress={() => {this.props.navigation.navigate('Daily')}} underlayColor="white">
                 <View style={styles.button}>
                   <Text style={styles.buttonText}>Daily</Text>
                 </View>
               </TouchableHighlight>
-              {/* <Button
-                title="Daily"
-                onPress={() => {this.props.navigation.navigate('Daily')}}
-              /> */}
+           
               <TouchableHighlight onPress={() => {this.props.navigation.navigate("Weekly")}} underlayColor="white">
                 <View style={styles.button}>
                   <Text style={styles.buttonText}>Weekly</Text>
                 </View>
               </TouchableHighlight>
-              {/* <Button 
-              title="Weekly"
-              onPress={() => {this.props.navigation.navigate("Weekly")}}/> */}
+       
               <Button
                 color="#442D2D"
                 title="Login"
